cc3da3cd7ce984368e85aba0f64a441640152508
Vlad Kapustsin <kapustsin.u@titan-t.com>
2022-11-13 21:49:15 +0200

    auth init + routing 4 each module

---
+++ src/app/auth/auth.module.ts
@@ -0,0 +1,17 @@
+import { RouterModule, Routes } from '@angular/router'
+import { RegisterComponent } from './components/register/register.component'
+import { CommonModule } from '@angular/common'
+import { NgModule } from '@angular/core'
+
+const routes: Routes = [
+  {
+    path: 'register',
+    component: RegisterComponent
+  }
+]
+
+@NgModule({
+  imports: [CommonModule, RouterModule.forChild(routes)],
+  declarations: [RegisterComponent]
+})
+export class AuthModule {}
